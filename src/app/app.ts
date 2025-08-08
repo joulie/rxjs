f3ec934b7237cafc460b991edc4e0128d7a0c8b3
joulie <ajoulie@live.fr>
2025-08-08 15:48:17 +0200

    observables dans la console

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal, VERSION } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {of, from} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  name = 'Angular ' + VERSION.major;
   protected readonly title = signal('rxjs');
+
+  ngOnInit(): void {
+    of(2,4,6,8).subscribe(item => console.log(item));
+
+    from([1,2,3,4]).subscribe({
+      next: item => console.log(`resulting item .. ${item}`),
+      error: err => console.error('Error:', err),
+      complete: () => console.log('Completed')
+    });
+
+    of('Apple1', 'Apple2', 'Apple3').subscribe({
+      next: (apple) => console.log(`Apple emitted: ${apple}`),
+      error: err => console.error('Error:', err),
+      complete: () => console.log('Completed of()')
+    });
+  }
 }
